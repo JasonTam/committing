91fb42d0f2eca155529d08ed72396f3eb2d38110
Jason Tam <JasonTam22@gmail.com>
2013-09-28 19:16:44 -0400

    added scrape to user profiles to get github profiles (did not filter empty profiles yet)

--- userScrape.js
+++ userScrape.js
@@ -5,20 +5,31 @@ var baseUrl = 'http://www.hackerleague.org';
 var partsUrl = baseUrl + '/hackathons/fall-2013-hackny-student-hackathon/participations';
 
 
-var scrapeUser = function() {
+var scrapeUser = function(userPageUrl) {
 
-};
+	request(userPageUrl, function(err, resp, body){
+		var $ = cheerio.load(body);
+
+		//Git Profile Links
+		gitLinks = $('.contact_row > a.github.icon'); 
+		$(gitLinks).each(function(i, gitLink){
+		var gitUrl = $(gitLink).attr('href');
+		console.log(gitUrl);
+		});
+	});
+
+}
 
 request(partsUrl, function(err, resp, body){
-  $ = cheerio.load(body);
-  
-  //User Page Links
-  userLinks = $('.user a'); 
-  $(userLinks).each(function(i, userLink){
+	var $ = cheerio.load(body);
+
+	//User Page Links
+	userLinks = $('.user > .details > a'); 
+	$(userLinks).each(function(i, userLink){
 	var userPageUrl = baseUrl + $(userLink).attr('href');
 	console.log(userPageUrl);
 	scrapeUser(userPageUrl)
-  });
+	});
 
 });
 

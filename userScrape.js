0d5075a2e3fac6a925c4a1fa57288a33baebbefc
Jason Tam <JasonTam22@gmail.com>
2013-09-28 18:42:35 -0400

    Init commit

---
+++ userScrape.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var baseUrl = 'http://www.hackerleague.org';
+var partsUrl = baseUrl + '/hackathons/fall-2013-hackny-student-hackathon/participations';
+
+
+var scrapeUser = function() {
+
+};
+
+request(partsUrl, function(err, resp, body){
+  $ = cheerio.load(body);
+  
+  //User Page Links
+  userLinks = $('.user a'); 
+  $(userLinks).each(function(i, userLink){
+	var userPageUrl = baseUrl + $(userLink).attr('href');
+	console.log(userPageUrl);
+	scrapeUser(userPageUrl)
+  });
+
+});
+

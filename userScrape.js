b9ada83e1afe12d8a7ef22995534d7e3af269fc9
Jason Tam <JasonTam22@gmail.com>
2013-09-28 20:04:14 -0400

    fixed base url

--- userScrape.js
+++ userScrape.js
@@ -1,8 +1,8 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var hlBaseUrl = 'http://www.hackerleague.org/';
-var partsUrl = hlBaseUrl + 'hackathons/fall-2013-hackny-student-hackathon/participations';
+var hlBaseUrl = 'http://www.hackerleague.org';
+var partsUrl = hlBaseUrl + '/hackathons/fall-2013-hackny-student-hackathon/participations';
 var ghBaseUrl = 'http://www.github.com/';
 
 // emitter.setMaxListeners(0);
